df995e825f6f9137fb2e0cbb73b3e26e341ba584
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2022-10-02 14:04:40 -0300

    angular autotypes form example

--- src/app/pages/components/about/about.component.ts
+++ src/app/pages/components/about/about.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
+  constructor(private formBuilder: FormBuilder) {}
 
-  constructor() { }
+  public form = this.formBuilder.group({
+    email: ['teste@teste'],
+    password: [''],
+  })
 
   ngOnInit(): void {
+    console.log(this.form.value.email)
   }
-
 }
