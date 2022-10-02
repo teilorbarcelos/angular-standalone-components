901402b5a78a475dd5f5eda60151fbf3915dcde0
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2022-10-02 13:56:10 -0300

    home and about pages added and working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
 import { RouterModule } from '@angular/router'
+import { HeaderComponent } from './core/components/header/header.component'
 
 @Component({
   selector: 'app-root',
   template: `
+    <app-header></app-header>
     <router-outlet></router-outlet>
   `,
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, HeaderComponent],
 })
 export class AppComponent {
   title = 'angular-standalone'
